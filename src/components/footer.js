2b0d71aa6e14e1372ffed3ef308553120d272776
Brian078Zhang <wohencool21@gmail.com>
2020-09-26 14:07:19 +0930

    footer update

--- src/components/footer.js
+++ src/components/footer.js
@@ -102,23 +102,10 @@ const Footer = () => {
       </StyledSocial>
       <StyledMetadata tabindex="-1">
         <StyledGitHubLink
-          href="https://github.com/bchiang7/v4"
+          href="https://github.com/Brian078Zhang/Portfolio"
           target="_blank"
           rel="nofollow noopener noreferrer">
           <div>Designed &amp; Built by Brian Zhang</div>
-
-          {githubInfo.stars && githubInfo.forks && (
-            <StyledGitHubInfo>
-              <span>
-                <FormattedIcon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <FormattedIcon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </StyledGitHubInfo>
-          )}
         </StyledGitHubLink>
       </StyledMetadata>
     </StyledContainer>
